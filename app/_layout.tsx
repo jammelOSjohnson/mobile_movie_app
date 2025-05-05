0a2065446c3a944570cd57d116a09eabb06bb96c
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-05-05 11:52:55 -0500

    Add StatusBar component to RootLayout for improved UI control

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { StatusBar } from "react-native";
+import React from "react";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
